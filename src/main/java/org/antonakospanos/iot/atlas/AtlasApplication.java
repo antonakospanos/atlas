aaa1a727665d00a50a2610884e67132b9d76abfb
Panos Antonakos <panos.antonakos@upstreamsystems.com>
2017-11-19 18:14:25 +0200

    FIX: Dependencies

--- src/main/java/org/antonakospanos/iot/atlas/AtlasApplication.java
+++ src/main/java/org/antonakospanos/iot/atlas/AtlasApplication.java
@@ -1,83 +1,26 @@
 package org.antonakospanos.iot.atlas;
 
-@SpringBootApplication(exclude = {
-		DataSourceAutoConfiguration.class,
-		JmsAutoConfiguration.class,
-		EmbeddedServletContainerAutoConfiguration.class
-})
-@ComponentScan(basePackages = { "com.upstreamsystems.curry" })
-@ImportResource("/WEB-INF/curry-interface-context.xml")
+import com.google.common.collect.ImmutableMap;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"org.antonakospanos.iot.atlas"})
 public class AtlasApplication extends SpringBootServletInitializer {
-	
-	private static final String CONFIG_NAME = "curry-interface-application";
 
-	@Value("${application.configuration.file-location}")
-	String fileLocation;
+	private static final String CONFIG_NAME = "application-atlas";
 
-	@Value("${application.configuration.smpp-channels-file-location}")
-	String smppChannelsFileLocation;
-	
-	@Value("${application.configuration.queue-management-file-location}")
-	String queueManagementFileLocation;
-
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(CurryApiApplication.class).
-				properties(ImmutableMap.of("spring.config.name", CONFIG_NAME))
-				.build()
-				.run(args);
-	}
-	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.properties(ImmutableMap.of("spring.config.name", CONFIG_NAME));
 	}
 
-	@Bean
-	public FlowsManager flowsManager() {
-		FlowsManager flowsManager = new FlowsYamlManager(this.fileLocation);
-		return flowsManager;
-	}
-
-	@Bean
-	public SmppChannelsManager smppChannelsManager() {
-		SmppChannelsManager smppChannelsManager = new SmppChannelsYamlManager(this.smppChannelsFileLocation);
-		return smppChannelsManager;
-	}
-	
-	@Bean
-	public QueueManagementManager queueManagementManager() {
-		QueueManagementManager queueManagementManager = new QueueManagementYamlManager(this.queueManagementFileLocation);
-		return queueManagementManager;
-	}
-	
-	/**
-	 * @return MultipartFile Resolver
-	 */
-	@Bean
-	public CommonsMultipartResolver multipartResolver() {
-		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
-		multipartResolver.setMaxUploadSize(5000000);
-		
-		return multipartResolver;
-	}
-	
-	/**
-	* @return File Upload Filter
-	*/
-	@Bean
-	public FilterRegistrationBean openEntityManagerFilterRegistrationBean() {
-		final MultipartFilter multipartFilter = new MultipartFilter();
-		final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(multipartFilter);
-		filterRegistrationBean.addInitParameter("multipartResolverBeanName", "commonsMultipartResolver");
-
-		return filterRegistrationBean;
-	}
-
-	public String getFileLocation() {
-		return this.fileLocation;
-	}
-
-	public void setFileLocation(String fileLocation) {
-		this.fileLocation = fileLocation;
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(AtlasApplication.class).
+				properties(ImmutableMap.of("spring.config.name", CONFIG_NAME))
+				.build()
+				.run(args);
 	}
 }
--- src/main/java/org/antonakospanos/iot/atlas/configuration/SwaggerConfiguration.java
+++ src/main/java/org/antonakospanos/iot/atlas/configuration/SwaggerConfiguration.java
@@ -1,11 +1,8 @@
 package org.antonakospanos.iot.atlas.configuration;
 
 import com.google.common.base.Predicate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
@@ -17,23 +14,12 @@ import static springfox.documentation.builders.PathSelectors.regex;
 @Configuration
 // @EnableAutoConfiguration
 @EnableSwagger2
-@ComponentScan(basePackages = { "com.upstreamsystems.curry.api" })
 public class SwaggerConfiguration {
-
-	@Autowired
-	Environment env;
-
-	// @Bean
-	// public Docket swaggerSpringMvcPlugin() {
-	// return new Docket(DocumentationType.SWAGGER_2)
-	// .groupName("curry-interface")
-	// .apiInfo(apiInfo());
-	// }
 
 	@Bean
 	public Docket newsApi() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.groupName("curry-interface")
+				.groupName("Internet-of-Things")
 				.apiInfo(apiInfo())
 				.select()
 				.paths(paths())
@@ -42,14 +28,14 @@ public class SwaggerConfiguration {
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
-				.title("Curry Interface")
-				//.contact("Curry Dev Team")
-				.description("An interface to Curry Gateway's configuration")
+				.title("Atlas")
+				.contact("itac developments")
+				.description("API handling events from Internet of Things (IoT) devices")
 				.build();
 	}
 
 	private Predicate<String> paths() {
-		return regex(".*/(flows|file|forms|schemas|version|throttling|queues).*");
+		return regex(".*/(devices|events|actions).*");
 		// return or(regex("/*"));
 	}
 }
