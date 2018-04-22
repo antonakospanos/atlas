bba32d455de5a17c4e94fc73ca6cd78331586025
Panos Antonakos <antonakospanos@gmail.com>
2018-04-23 02:40:40 +0300

    ADD: Content-Length on Atlas' responses

--- src/main/java/org/antonakospanos/iot/atlas/web/configuration/CORSFilter.java
+++ src/main/java/org/antonakospanos/iot/atlas/web/configuration/CORSFilter.java
@@ -13,7 +13,7 @@ public class CORSFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		response.setHeader("Access-Control-Allow-Origin", "*");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
-		response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+		response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, PATCH, OPTIONS, DELETE");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers",
 				"DNT,X-Mx-ReqToken,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type");
